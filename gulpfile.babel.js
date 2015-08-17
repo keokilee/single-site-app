6552a440f35b8c8d8f7da429a7984db79d38898c
George Lee <keoki.lee@gmail.com>
2015-08-16 21:17:26 -1000

    Initial PostCSS setup.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp';
 import plugins from 'gulp-load-plugins';
+import autoprefixer from 'autoprefixer';
+import cssnext from 'cssnext';
 
-let {babel, run, rename, jsxcs} = plugins();
+let {babel, run, rename, jsxcs, postcss} = plugins();
 
 gulp.task('compile:js', ['jscs'], () => {
   return gulp.src('app/scripts/index.es6.js')
@@ -13,13 +15,26 @@ gulp.task('compile:js', ['jscs'], () => {
 gulp.task('jscs', () => {
   return gulp.src(['app/scripts/*', 'browser/scripts/*'])
              .pipe(jsxcs({
-               preset: "node-style-guide",
-               esnext: true
+               preset: 'node-style-guide',
+               esnext: true,
              }));
 });
+
+gulp.task('compile:css', () => {
+  let processors = [
+    autoprefixer({browsers: ['last 1 version']}),
+    cssnext(),
+  ];
+
+  return gulp.src('app/styles/*.css')
+             .pipe(postcss(processors))
+             .pipe(gulp.dest('browser/styles'));
+});
+
+gulp.task('compile', ['compile:css', 'compile:js']);
 
 gulp.task('run', ['default'], () => {
   return run('electron .').exec();
 });
 
-gulp.task('default', ['compile:js']);
+gulp.task('default', ['compile']);
