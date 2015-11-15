cc18880907c006d8ade48757bdcb812594f287ec
George Lee <keoki.lee@gmail.com>
2015-11-14 17:25:10 -1000

    Finish tests for header.

--- spec/browser/components/Header_spec.jsx
+++ spec/browser/components/Header_spec.jsx
@@ -1,60 +1,144 @@
 import React from 'react';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithTag
+  scryRenderedDOMComponentsWithTag,
+  Simulate
 } from 'react-addons-test-utils';
 
-import expect from 'expect';
+import expect, { spyOn } from 'expect';
 import Header from 'app/components/Header';
 
 describe('Header', () => {
-  describe('buttons', () => {
-    it('has four buttons', () => {
-      const component = renderIntoDocument(
-        <Header enableBack={() => true} enableForward={() => true} />
-      );
+  const headerProps = {
+    enableBack: () => true,
+    enableForward: () => true
+  };
 
-      const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+  const renderHeader = () => {
+    return renderIntoDocument(
+      <Header {...headerProps} />
+    );
+  };
+
+  describe('url', () => {
+    it('displays the url from props', () => {
+      const url = 'http://www.github.com';
+      headerProps.url = url;
+      const header = renderHeader();
+
+      // div is the third one
+      const titleBar = scryRenderedDOMComponentsWithTag(header, 'div')[2];
+      expect(titleBar.props.children).toEqual(url);
+    });
+  });
+
+  describe('buttons', () => {
+    const renderButtons = () => {
+      const component = renderHeader();
+      return scryRenderedDOMComponentsWithTag(component, 'button');
+    };
+
+    it('has four buttons', () => {
+      const buttons = renderButtons();
       expect(buttons.length).toEqual(4);
     });
 
     describe('back', () => {
       it('is not disabled if enableBack returns true', () => {
-        const component = renderIntoDocument(
-          <Header enableBack={() => true} enableForward={() => true} />
-        );
-
-        const backButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        headerProps.enableBack = () => true;
+        const backButton = renderButtons()[0];
         expect(backButton.disabled).toBe(false);
       });
 
       it('is disabled if enableBack returns false', () => {
-        const component = renderIntoDocument(
-          <Header enableBack={() => false} enableForward={() => true} />
-        );
+        headerProps.enableBack = () => false;
+        const backButton = renderButtons()[0];
 
-        const backButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
         expect(backButton.disabled).toBe(true);
+      });
+
+      it('triggers the onBack function if the button is clicked', () => {
+        headerProps.enableBack = () => true;
+        headerProps.onBack = () => 'yo';
+        const spy = spyOn(headerProps, 'onBack');
+
+        const backButton = renderButtons()[0];
+        Simulate.click(backButton);
+        expect(spy).toHaveBeenCalled();
       });
     });
 
     describe('forward', () => {
       it('is not disabled if enableForward returns true', () => {
-        const component = renderIntoDocument(
-          <Header enableBack={() => true} enableForward={() => true} />
-        );
+        headerProps.enableForward = () => true;
 
-        const forwardButton = scryRenderedDOMComponentsWithTag(component, 'button')[1];
+        const forwardButton = renderButtons()[1];
         expect(forwardButton.disabled).toBe(false);
       });
 
       it('is disabled if enableForward returns false', () => {
-        const component = renderIntoDocument(
-          <Header enableBack={() => false} enableForward={() => false} />
-        );
+        headerProps.enableForward = () => false;
 
-        const forwardButton = scryRenderedDOMComponentsWithTag(component, 'button')[1];
+        const forwardButton = renderButtons()[1];
         expect(forwardButton.disabled).toBe(true);
+      });
+
+      it('triggers the onForward function if the button is clicked', () => {
+        headerProps.enableForward = () => true;
+        headerProps.onForward = () => 'yo';
+        const spy = spyOn(headerProps, 'onForward');
+
+        const forwardButton = renderButtons()[1];
+        Simulate.click(forwardButton);
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+
+    describe('refresh', () => {
+      it('is not hidden if the page is not loading', () => {
+        headerProps.loading = false;
+        const refreshButton = renderButtons()[2];
+        expect(refreshButton.hidden).toBe(false);
+      });
+
+      it('is hidden if the page is loading', () => {
+        headerProps.loading = true;
+        const refreshButton = renderButtons()[2];
+        expect(refreshButton.hidden).toBe(true);
+      });
+
+      it('triggers the onRefresh function if the button is clicked', () => {
+        headerProps.loading = false;
+        headerProps.onRefresh = () => 'yo';
+        const spy = spyOn(headerProps, 'onRefresh');
+
+        const refreshButton = renderButtons()[2];
+        Simulate.click(refreshButton);
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+
+    describe('stop', () => {
+      it('is not hidden if the page is loading', () => {
+        headerProps.loading = true;
+        const stopButton = renderButtons()[3];
+        expect(stopButton.hidden).toBe(false);
+      });
+
+      it('is hidden if the page is not loading', () => {
+        headerProps.loading = false;
+        const stopButton = renderButtons()[3];
+        expect(stopButton.hidden).toBe(true);
+      });
+
+      it('triggers the onStop function if the button is clicked', () => {
+        headerProps.loading = true;
+        headerProps.onStop = () => 'yo';
+        const spy = spyOn(headerProps, 'onStop');
+
+        const stopButton = renderButtons()[3];
+        Simulate.click(stopButton);
+        expect(spy).toHaveBeenCalled();
       });
     });
   });
