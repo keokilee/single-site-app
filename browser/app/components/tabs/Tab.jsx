1dc553a987e2c14e87175445b4ccafe168fbb4b8
George Lee <keoki.lee@gmail.com>
2015-11-19 22:58:13 -1000

    Add some styles.

--- browser/app/components/tabs/Tab.jsx
+++ browser/app/components/tabs/Tab.jsx
@@ -1,15 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
+import classnames from 'classnames';
 
 import styles from 'styles/tabs.css';
 
-@CSSModules(styles)
+@CSSModules(styles, { allowMultiple: true })
 export default class Tab extends Component {
   render() {
-    const { title } = this.props;
+    const { title, active } = this.props;
+    const classes = classnames('tab', {'active': active});
 
     return (
-      <div styleName='tab'>
+      <div styleName={classes}>
         {title}
         <button>x</button>
       </div>
